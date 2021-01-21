ac965aeab146d620ab234cf7c5fb5e90690c3856
savely-s <solomatov.saveliy@bk.ru>
2021-01-21 19:27:26 +0700

    дз и классная работа 1

--- src/fly.kt
+++ src/fly.kt
@@ -3,6 +3,6 @@ fun main(){
   var Nedelya = readLine()!!.toInt()
   var Year = readLine()!!.toInt()
   var a = Random.nextInt(1,8)
-    //решить задачу н 30 и люб на выборa
-
+    //решить задачу н 30 и люб на выбор
+//
 }
