2f0101c887f344fd016db49e5a4de54c9d6af3cf
ImPaha <56339635+ImPaha@users.noreply.github.com>
2021-01-21 19:19:55 +0700

    дз и классная работа

--- src/ly.kt
+++ src/ly.kt
@@ -1,9 +1,12 @@
 fun main(){
-    var a = 100
-    println("100 = $a")
-    var b = 200
-    println(a)
-    println("200 = $b")
+    var V1 = readLine()!!.toInt()
+    var V2 = readLine()!!.toInt()
+    var S = readLine()!!.toInt()
+    var T = readLine()!!.toInt()
+    var p = V1 + V2
+    println("суммарная скорость: $p")
+    var c = T * p
+    println("Общий путь: $S")
+    println("время умноженное на сумарное время: $c")
 
-    println(b)
 }
